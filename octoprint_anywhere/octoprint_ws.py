1da2066b2a1024cc10f4af250e24fb331312a1ed
Kenneth Jiang <kenneth.jiang@gmail.com>
2017-06-26 15:04:54 -0700

    octoprint websocket client for 1.3.4

--- octoprint_anywhere/octoprint_ws.py
+++ octoprint_anywhere/octoprint_ws.py
@@ -29,9 +29,22 @@ def listen_to_octoprint(settings, q):
         __deplete_queue__(q)
         q.put(json.dumps(message_payload))
 
-    octoprint_client.init_client(settings)
-    socket = octoprint_client.connect_socket(on_connect=on_connect,
+    if "init_client" in dir(octoprint_client): # OctoPrint <= 1.3.2
+        octoprint_client.init_client(settings)
+        socket = octoprint_client.connect_socket(on_connect=on_connect,
                                              on_close=on_close,
                                              on_error=on_error,
                                              on_heartbeat=on_heartbeat,
                                              on_message=on_message)
+    else:
+        host = settings.get(["server", "host"])
+        host = host if host != "0.0.0.0" else "127.0.0.1"
+        port = settings.getInt(["server", "port"])
+        apikey = settings.get(["api", "key"])
+        baseurl = octoprint_client.build_base_url(host=host, port=port)
+        client = octoprint_client.Client(baseurl, apikey)
+        client.create_socket(on_connect=on_connect,
+                 on_close=on_close,
+                 on_error=on_error,
+                 on_heartbeat=on_heartbeat,
+                 on_message=on_message)
