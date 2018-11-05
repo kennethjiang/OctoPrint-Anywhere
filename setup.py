03d23c70da9d896ceee8e32718f6edd38e80efff
Kenneth Jiang <kenneth.jiang@gmail.com>
2018-11-05 14:11:13 -0800

    bump version: 0.9.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_anywhere"
 plugin_name = "OctoPrint Anywhere"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.8.8"
+plugin_version = "0.9.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
