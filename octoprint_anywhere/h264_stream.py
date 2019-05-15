3f36dc18214ca03487c3a58f6b6d7cd5e5ca1e5c
Kenneth Jiang <kenneth.jiang@gmail.com>
2019-05-15 04:06:48 +0100

    wait for 4 extra seconds after watching is over

--- octoprint_anywhere/h264_stream.py
+++ octoprint_anywhere/h264_stream.py
@@ -152,6 +152,7 @@ class H264Streamer:
                 self.camera.start_recording(sub_proc.stdin, format='h264', quality=23)
                 while remote_status['watching']:
                     self.camera.wait_recording(2)
+                self.camera.wait_recording(4)   # record 4 more seconds to minimize the pause user views the stream again
                 self.camera.stop_recording()
                 self.camera.start_preview()
             else:
