defb9329339610b9c00eba11ee1e3a791d65c569
Kenneth Jiang <kenneth.jiang@gmail.com>
2019-06-29 07:38:40 -0700

    option to hide picamera warning

--- octoprint_anywhere/static/js/anywhere.js
+++ octoprint_anywhere/static/js/anywhere.js
@@ -8,6 +8,8 @@
 (function () {
 'use strict';
 
+PNotify.prototype.options.confirm.buttons = [];
+
 function AnywhereViewModel(parameters) {
     var self = this;
 
@@ -29,12 +31,29 @@ function AnywhereViewModel(parameters) {
     var setConfigVars = function(configResp) {
         self.regUrl(configResp.reg_url);
         self.registered(configResp.registered);
-        if (configResp.picamera_error) {
+        var picameraErrorAcked = localStorage.getItem("octoprint_anywhere.picameraErrorAcked");
+        if (configResp.picamera_error) && !picameraErrorAcked) {
             new PNotify({
                 title: "OctoPrint Anywhere",
                 text: "<p>Failed to detect and turn on Pi Camera. Webcam feed will be streaming at 3 FPS. If you want 24 FPS streaming, please make sure Pi Camera is plugged in correctly.</p><a href='https://www.getanywhere.io/assets/oa10.html#picamera'>Learn more >>></a>",
-                type: "error",
+                type: "warning",
                 hide: false,
+                confirm: {
+                    confirm: true,
+                    buttons: [
+                        {
+                            text: "Ignore",
+                            click: function(notice) {
+                                localStorage.setItem("octoprint_anywhere.picameraErrorAcked", true);
+                                notice.remove();
+                            }
+                        },
+                    ]
+                },
+                buttons: {
+                    closer: false,
+                    sticker: false
+                },
             });
         }
     };
