532982c8af4b716d4b349f5620b04f1b8fba866d
Kenneth Jiang <kenneth.jiang@gmail.com>
2019-08-02 05:58:23 -0700

    require restart after token is reset

--- octoprint_anywhere/__init__.py
+++ octoprint_anywhere/__init__.py
@@ -66,10 +66,6 @@ class AnywherePlugin(octoprint.plugin.SettingsPlugin,
         if command == "reset_config":
             old_token = self.get_config()['token']
             self.get_config().reset_config()
-
-            self.main_loop.quit()
-            self.start_main_thread()
-
             return flask.jsonify(reg_url="{0}/pub/link_printer?token={1}&copy_from={2}".format(self.get_config()['api_host'], self.get_config()['token'], old_token), registered=self.get_config()['registered'])
         elif command == "get_config":
             conf = self.get_config().as_dict()
